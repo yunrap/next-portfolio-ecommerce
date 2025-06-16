03a07f1c7f8692d39ddf6d7588a926dde3e08281
yunrap <dbsdpwl7@gmail.com>
2025-06-16 18:07:31 +0900

    feat: 초기 프로젝트 셋업 및 글로벌 스타일/폰트/메타데이터 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended',
+  ),
+  {
+    files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    plugins: {
+      tailwindcss: 'eslint-plugin-tailwindcss',
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'tailwindcss/classnames-order': 'warn',
+    },
+  },
 ];
 
 export default eslintConfig;
