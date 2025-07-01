c6b8915f3a36fd3e7460f13f0a0f1ee6c8c53573
yunrap <dbsdpwl7@gmail.com>
2025-07-01 14:21:45 +0900

    fix: useSearchParams 오류 해결 및 Suspense 적용

--- src/app/(routes)/(shop)/product/page.tsx
+++ src/app/(routes)/(shop)/product/page.tsx
@@ -1,9 +1,10 @@
 import ProductPage from '@/app/domains/shop/product/ProductPage';
+import { Suspense } from 'react';
 
 export default function Product() {
   return (
-    <>
+    <Suspense fallback={<div>로딩 중...</div>}>
       <ProductPage />
-    </>
+    </Suspense>
   );
 }
