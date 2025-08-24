6aeeb96dbec83038e817ff499a8dd01ec04a0a03
yunrap <dbsdpwl7@gmail.com>
2025-08-25 00:14:23 +0900

    fix: useParams 사용으로인한 suspense 추가, 서버컴포넌트 변경예정

--- src/app/(routes)/(shop)/product/page.tsx
+++ src/app/(routes)/(shop)/product/page.tsx
@@ -1,5 +1,16 @@
 import ProductPage from '@/app/domains/shop/product/ProductPage';
+import { Suspense } from 'react';
 
 export default function Product() {
-  return <ProductPage />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex min-h-screen w-full items-center justify-center">
+          <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-b-4 border-gray-300"></div>
+        </div>
+      }
+    >
+      <ProductPage />
+    </Suspense>
+  );
 }
